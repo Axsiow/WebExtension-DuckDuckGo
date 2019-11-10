556c3370a91232d2bbb25122158bd29aeebc7d3f
Axsiow <TehSiow2@gmail.com>
2019-11-10 03:12:14 +0100

    Update duckduckgo.js

--- duckduckgo.js
+++ duckduckgo.js
@@ -9,7 +9,7 @@ if(timeToDark.getHours() >= 0 || timeToDark.getHours() <= 20){ // Fonction qui f
 	document.body.style.backgroundColor = "black"; // Affiche le background de la page en noir
 }
 
-colorLinks("#B894FF"); // définie la couleur en #B894FF (violet)
+colorLinks("#B894FF"); // Définie la couleur des liens en #B894FF (violet)
 
 function colorLinks(hex){
 
@@ -25,14 +25,14 @@ document.body.onload = addElement;
 
 function addElement () {
 
-  var newDiv = document.createElement("p");  // crée un nouvel élément p
+  var newDiv = document.createElement("p");  // Crée un nouvel élément p
  
   var newContent = document.createTextNode("Créer par Enzo F. Utilisez DuckDuckGo, Ils ont pas de scandales, comparé à Qwant et Google !");
   
   newDiv.appendChild(newContent);
   
-  var currentDiv = document.getElementById("p1"); // ajoute le nouvel élément créé et son contenu dans le DOM
+  var currentDiv = document.getElementById("p1"); // Add le nouvel élément créé et son contenu dans le DOM
   document.body.insertBefore(newDiv, currentDiv);
 
-  document.body.style.color = "white"; // Mes la couleur en blanc
+  document.body.style.color = "white"; // Fix la couleur en blanc
 }
