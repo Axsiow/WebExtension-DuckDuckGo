707128dc7281dd70515d7f162310e22d1ffdf623
Axsiow <TehSiow2@gmail.com>
2019-11-10 01:15:47 +0100

    fuck js

--- twitter.js
+++ twitter.js
@@ -1,4 +1,13 @@
 /*
-Just draw a border round the document.body.
+BONSOIR PARIS
 */
-document.body.style.border = "5px solid red";
+
+var timeToDark = new Date();
+
+if(timeToDark.getHours() >= 0 || timeToDark.getHours() <= 20){
+	document.body.style.border = "5px solid blue";
+	// document.getElementsByClassName("page-wrapper").style.backgroundColor = "#1f1f1f";
+	document.body.style.backgroundColor = "black";
+}
+
+
