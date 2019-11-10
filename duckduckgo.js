1eb356038bba92a3a74b02b65d0cfbdf2b1b7f52
Axsiow <TehSiow2@gmail.com>
2019-11-10 01:35:18 +0100

    fix

--- duckduckgo.js
+++ duckduckgo.js
@@ -5,8 +5,6 @@ BONSOIR PARIS
 var timeToDark = new Date();
 
 if(timeToDark.getHours() >= 0 || timeToDark.getHours() <= 20){
-	document.body.style.border = "5px solid blue";
-	// document.getElementsByClassName("page-wrapper").style.backgroundColor = "#1f1f1f";
 	document.body.style.backgroundColor = "black";
 }
 
